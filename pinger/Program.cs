978bfd149f90d5f594b6c4293d824bace88c7b94
kasmann <damneda@gmail.com>
2019-04-15 14:24:32 +0300

    test http request

--- pinger/Program.cs
+++ pinger/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,18 +11,47 @@ namespace pinger
     class Program
     {
         static void Main(string[] args)
-        { }
+        {
+            WebRequest request = WebRequest.Create("http://ya.ru");
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+            Console.Read();
+        }
     }
 
     class Pinger
-    { }
+    {
+        public PingAnswer Answer;
+        public void send() { }
+        //отправляем на указанный адрес указанный пакет
+    }
 
     class Logger
-    { }
+    {
+        //пишем в лог
+    }
 
     class PingAnswer
-    { }
+    {
+        //получаем ответ запрашиваемого ресурса
+        //умеем его парсить
+    }
 
     class Query
-    { }
+    {
+        //формируем пакет по типу протокола
+    }
+
+    class Configuration
+    {
+        //чтение файла конфигурации, парсинг нужных значений
+        //если в строке больше одного значения, то предупреждаем, но продолжаем работать с первым
+    }
 }
